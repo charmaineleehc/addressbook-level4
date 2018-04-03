dc03828a9df5e2e4a22ed7a465ed4fe5e8bbfde9
charmaineleehc <e0052642@u.nus.edu>
2018-04-04 00:29:15 +0800

    remove import statements

--- src/test/java/seedu/carvicim/logic/commands/EmailCommandTest.java
+++ src/test/java/seedu/carvicim/logic/commands/EmailCommandTest.java
@@ -1,15 +1,5 @@
 package seedu.carvicim.logic.commands;
 
-import static org.junit.Assert.assertEquals;
-import static seedu.carvicim.testutil.TypicalEmployees.getTypicalCarvicim;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import seedu.carvicim.model.Model;
-import seedu.carvicim.model.ModelManager;
-import seedu.carvicim.model.UserPrefs;
-
 public class EmailCommandTest {
 
     /*private Model model;
