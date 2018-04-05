7288e13bd401c0752fcd232295653057aac0ae7d
charmaineleehc <e0052642@u.nus.edu>
2018-04-05 22:47:41 +0800

    emailcommand test case

--- src/test/java/seedu/carvicim/logic/commands/EmailCommandTest.java
+++ src/test/java/seedu/carvicim/logic/commands/EmailCommandTest.java
@@ -1,8 +1,17 @@
 package seedu.carvicim.logic.commands;
 
+import org.junit.Before;
+import org.junit.Test;
+import seedu.carvicim.model.Model;
+import seedu.carvicim.model.ModelManager;
+import seedu.carvicim.model.UserPrefs;
+
+import static org.junit.Assert.assertEquals;
+import static seedu.carvicim.testutil.TypicalEmployees.getTypicalCarvicim;
+
 public class EmailCommandTest {
 
-    /*private Model model;
+    private Model model;
 
     @Before
     public void setUp() {
@@ -14,6 +23,6 @@ public class EmailCommandTest {
         EmailCommand emailCommand = new EmailCommand();
         CommandResult commandResult = emailCommand.executeUndoableCommand();
         assertEquals(EmailCommand.MESSAGE_SUCCESS, commandResult.feedbackToUser);
-    }*/
+    }
 
 }
