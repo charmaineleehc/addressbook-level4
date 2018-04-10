817500c9a1d28ec26e50c46f150cb36b9deffe9f
charmaineleehc <e0052642@u.nus.edu>
2018-04-11 01:58:00 +0800

    add 1 parser test

--- src/test/java/seedu/carvicim/logic/commands/FindByTagCommandTest.java
+++ src/test/java/seedu/carvicim/logic/commands/FindByTagCommandTest.java
@@ -14,8 +14,8 @@ import static seedu.carvicim.testutil.TypicalEmployees.GEORGE;
 import static seedu.carvicim.testutil.TypicalEmployees.getTypicalCarvicim;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Collections;
+import java.util.List;
 
 import org.junit.Test;
 
